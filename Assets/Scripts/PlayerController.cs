e266e22cbcaba4c77954829a42b8c3d5a5e2c448
Daniel Nordahl <danielnordahl93@gmail.com>
2018-11-26 15:30:27 +0100

    Assets and basic player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+
+    Rigidbody2D rb;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+        float x = Input.GetAxisRaw("Horizontal " + gameObject.name) * speed;
+        rb.velocity = new Vector2( x, rb.velocity.y);
+	}
+}

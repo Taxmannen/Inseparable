349407d785e96cadd3628cc88d2e28eafdc2d199
Daniel Nordahl <danielnordahl93@gmail.com>
2018-11-26 15:41:04 +0100

    Basic rope

---
+++ Assets/Scripts/Rope.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Rope : MonoBehaviour {
+    float maxDistance = 3.5f;
+
+    Transform player1;
+    Transform player2;
+    LineRenderer line;
+    DistanceJoint2D joint;
+
+    void Start()
+    {
+        player1 = GameObject.Find("Player 1").transform;
+        player2 = GameObject.Find("Player 2").transform;
+        line = GetComponent<LineRenderer>();
+        joint = GetComponent<DistanceJoint2D>();
+
+        joint.distance = maxDistance;
+    }
+
+    void Update()
+    {
+        if (gameObject.name == "Player 1")
+        {
+            line.SetPosition(0, player1.position);
+            line.SetPosition(1, player2.position);
+            joint.connectedAnchor = player2.position;
+        }
+        else joint.connectedAnchor = player1.position;
+        joint.anchor = new Vector2(0, 0);
+    }
+}

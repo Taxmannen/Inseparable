79fd3207908b40fdb7f41a147ef377d894af0996
Adam <adam.torngren@hotmail.com>
2018-11-27 12:49:56 +0100

    Limited camera to X movement

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -15,11 +15,17 @@ public class CameraManager : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update() {
+    void _Update() {
+        float medianXPosition = (player1.position.x + player2.position.x) * 0.5f;
+
+        this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x * (1 - fractionFromNewPositon) + medianXPosition * fractionFromNewPositon,
+            this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+	}
+
+    void FixedUpdate()
+    {
         Vector3 medianPosition = (player1.position + player2.position) * 0.5f;
 
-        Vector3 newPosition = this.gameObject.transform.position * (1 - fractionFromNewPositon) + medianPosition * fractionFromNewPositon;
-        newPosition.z = -10;
-        this.gameObject.transform.position = newPosition;
-	}
+        transform.position = Vector3.Lerp(new Vector3(transform.position.x, transform.position.y, -10f), new Vector3(medianPosition.x, transform.position.y, -10f), 3f * Time.deltaTime);
+    }
 }

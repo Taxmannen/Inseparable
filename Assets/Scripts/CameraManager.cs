68ea95c59c0f45551f72cc9f6b2eaed64db6b7f7
Adam <adam.torngren@hotmail.com>
2018-11-27 13:20:06 +0100

    Added xMin

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraManager : MonoBehaviour {
+public class CameraManager : MonoBehaviour
+{
+
     Transform player1;
     Transform player2;
 
     public float fractionFromNewPositon;
+    public float minCameraX, maxCameraX;
 
     void Start()
     {
@@ -15,16 +18,21 @@ public class CameraManager : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void _Update() {
+    void _Update()
+    {
         float medianXPosition = (player1.position.x + player2.position.x) * 0.5f;
 
         this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x * (1 - fractionFromNewPositon) + medianXPosition * fractionFromNewPositon,
             this.gameObject.transform.position.y, this.gameObject.transform.position.z);
-	}
+    }
 
     void FixedUpdate()
     {
-        Vector3 medianPosition = (player1.position + player2.position) * 0.5f;
+        float x = (player1.position.x + player2.position.x) * 0.5f;
+        if ((player1.position.x + player2.position.x * 0.5f) < minCameraX)
+            x = minCameraX;
+
+        Vector3 medianPosition = new Vector3(x, ((player1.position + player2.position) * 0.5f).y, ((player1.position + player2.position) * 0.5f).z);
 
         transform.position = Vector3.Lerp(new Vector3(transform.position.x, transform.position.y, -10f), new Vector3(medianPosition.x, transform.position.y, -10f), 3f * Time.deltaTime);
     }

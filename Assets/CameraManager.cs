741f125593f70c5b3f85280d3579ec30ed3e03fb
Adam <adam.torngren@hotmail.com>
2018-11-27 11:49:04 +0100

    Added unlocked camera movement following players

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+    Transform player1;
+    Transform player2;
+
+    public float fractionFromNewPositon;
+
+    void Start()
+    {
+        player1 = GameObject.Find("Player 1").transform;
+        player2 = GameObject.Find("Player 2").transform;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 medianPosition = (player1.position + player2.position) * 0.5f;
+
+        Vector3 newPosition = this.gameObject.transform.position * (1 - fractionFromNewPositon) + medianPosition * fractionFromNewPositon;
+        newPosition.z = -10;
+        this.gameObject.transform.position = newPosition;
+	}
+}

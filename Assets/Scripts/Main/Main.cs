898e6c6c03165f5124ee9f6b235ea52810ccc0e9
Daniel Nordahl <danielnordahl93@gmail.com>
2018-11-27 14:06:37 +0100

    Main script and cleanup

---
+++ Assets/Scripts/Main/Main.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour {
+
+	void Start ()
+    {
+        Application.targetFrameRate = 60;
+	}
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel")) SceneManager.LoadScene("Main"); // For Debug!
+    }
+}

037745a0e52ad4235d4bbd575a6d41513c5965e3
Daniel Nordahl <danielnordahl93@gmail.com>
2018-11-27 10:48:18 +0100

    Ground Check and input for jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    public LayerMask groundLayer;
+    public bool drawGizmos;
     public float speed;
     public float fraction;
 
     Rigidbody2D rb;
+    bool grounded;
 
 	void Start ()
     {
@@ -15,7 +16,18 @@ public class PlayerController : MonoBehaviour {
 	
 	void Update ()
     {
+        grounded = Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y - 0.35f), new Vector2(0.5f, 0.1f), 0, groundLayer);
+
         float x = Input.GetAxisRaw("Horizontal " + gameObject.name) * speed;
-        rb.velocity = new Vector2(rb.velocity.x * (1f - fraction) + x * fraction, rb.velocity.y);
-	}
+       // rb.velocity = new Vector2(rb.velocity.x * (1f - fraction) + x * fraction, rb.velocity.y);
+        rb.AddForce(new Vector2(x, 0));
+    }
+
+    void OnDrawGizmos()
+    {
+        if (drawGizmos)
+        {
+            Gizmos.DrawCube(new Vector2(transform.position.x, transform.position.y - 0.35f), new Vector2(0.5f, 0.1f));
+        }    
+    }
 }

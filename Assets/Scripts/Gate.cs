817e3fb265e0e964bbd0b5dbe2edaff424e1943c
Daniel Nordahl <danielnordahl93@gmail.com>
2018-11-29 10:54:44 +0100

    Loading Screen & Pressure puzzle

---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+/* Made by Daniel */
+public class Gate : MonoBehaviour {
+    [Range(0.5f, 2f)]
+    public float speed;
+    public Vector2 endPos;
+    public bool drawGizmos;
+
+    PressurePlate pressurePlate;
+    PressurePlate pressurePlate1;
+
+
+	void Start ()
+    {
+        pressurePlate  = transform.parent.GetChild(1).GetComponent<PressurePlate>();	
+        pressurePlate1 = transform.parent.GetChild(2).GetComponent<PressurePlate>();	
+	}
+	
+	void Update ()
+    {
+		if (pressurePlate.on && pressurePlate1.on)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector2(transform.position.x + endPos.x, endPos.y), speed/100);
+        }
+	}
+
+    private void OnDrawGizmos()
+    {
+        if (drawGizmos) Gizmos.DrawCube(new Vector2(transform.position.x + endPos.x, endPos.y), transform.localScale);
+    }
+}
---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PressurePlate : MonoBehaviour {
+
+    public bool on;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            on = true;
+            transform.localScale = new Vector3(transform.localScale.x, 0.1f, transform.localScale.z);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            on = false;
+            transform.localScale = new Vector3(transform.localScale.x, 0.2f, transform.localScale.z);
+        }
+    }
+}

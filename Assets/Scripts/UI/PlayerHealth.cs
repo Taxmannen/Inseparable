b3707e72b99d9ac6b6f1b88ebc8cae11df898378
Unknown <jkm.svensson@gmail.com>
2018-11-29 13:49:41 +0100

    HealthBar, ItemIcon

---
+++ Assets/Scripts/UI/PlayerHealth.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+// Made by Jocke
+public class PlayerHealth : MonoBehaviour {
+    
+    public Slider healthBar;
+    public float CurrentHealth;
+    public float MaxHealth;
+
+
+    void Start () {
+        healthBar.maxValue = MaxHealth;
+        healthBar.value = CurrentHealth;
+	}
+
+    private void Update()
+    {
+        healthBar.value = CurrentHealth;
+    }
+}
---
+++ Assets/Scripts/UI/SelectedItem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+//Made by Jocke
+public class SelectedItem : MonoBehaviour {
+    Image selectedItem;
+    Sprite sprite;
+	void Start () {
+        selectedItem = GetComponent <Image>();
+        
+    }
+
+    private void Update()
+    {
+        SwapSprite(sprite);
+    }
+
+    public void SwapSprite(Sprite sprite)
+    {
+        selectedItem.sprite = this.sprite;
+    }
+}

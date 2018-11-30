e99e30cd0ad09251890dbc2d2f5e9c22b81e08b7
Daniel Nordahl <danielnordahl93@gmail.com>
2018-11-30 12:25:47 +0100

    Moving Obstacle

---
+++ Assets/Scripts/Pussel/MovingObstacle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MovingObstacle : MonoBehaviour {
+    public float endPosX;
+    public float speed;
+    public bool on;
+    public bool drawGizmos;
+
+	void Update ()
+    {
+		if (on)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector2(endPosX, transform.position.y), speed/100);
+        }
+	}
+
+    private void OnDrawGizmos()
+    {
+        if (drawGizmos)
+        {
+            Gizmos.DrawCube(new Vector2(endPosX, transform.position.y), transform.localScale);
+        }
+    }
+}

ce410f1dddaf93ca65c6eac7c363cd8ceb71e9b8
Daniel Nordahl <danielnordahl93@gmail.com>
2018-12-01 16:15:11 +0100

    Inputs added for all buttons

--- Assets/Scripts/Main/Main.cs
+++ Assets/Scripts/Main/Main.cs
@@ -8,4 +8,29 @@ public class Main : MonoBehaviour {
         Application.targetFrameRate = frameRate;
         QualitySettings.vSyncCount = 0;
     }
+
+    private void Update()
+    {
+        //ButtonTest("Player 1");
+        //ButtonTest("Player 2");
+    }
+
+    void ButtonTest(string player)
+    {
+        if (Input.GetButtonDown("Jump"     + " " + player)) Debug.Log("Jump");
+        if (Input.GetButtonDown("Attack"   + " " + player)) Debug.Log("Atack");
+        if (Input.GetButtonDown("Use Item" + " " + player)) Debug.Log("Use Item");
+        if (Input.GetButtonDown("Seperate" + " " + player)) Debug.Log("Seperate");
+
+
+        if (Input.GetAxisRaw("Pickup" + " " + player) != 0) Debug.Log("Pickup");
+        if (Input.GetAxisRaw("Throw"  + " " + player) != 0) Debug.Log("Throw");
+
+        if (Input.GetAxisRaw("Change Item" + " " + player) < 0) Debug.Log("Change Item -");
+        if (Input.GetAxisRaw("Change Item" + " " + player) > 0) Debug.Log("Change Item +");
+
+        if (Input.GetButtonDown("Inventory" + " " + player)) Debug.Log("Inventory");
+
+        if (Input.GetButtonDown("Menu")) Debug.Log("Menu");
+    }
 }

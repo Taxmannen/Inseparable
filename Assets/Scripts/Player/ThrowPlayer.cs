e0939053215b0fe99c056c26b0d513ba9eb468cf
Daniel Nordahl <danielnordahl93@gmail.com>
2018-12-03 15:33:25 +0100

    Scrolling background fix

--- Assets/Scripts/Player/ThrowPlayer.cs
+++ Assets/Scripts/Player/ThrowPlayer.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ThrowPlayer : MonoBehaviour {
+    public LayerMask layer;
+    public float affectedAreaSize;
+    public bool drawGizmos;
 
-	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        bool canPickup = Physics2D.OverlapCircle(transform.position, affectedAreaSize, layer);
 	}
+
+    private void OnDrawGizmos()
+    {
+        if (drawGizmos) Gizmos.DrawSphere(transform.position, affectedAreaSize);
+    }
 }

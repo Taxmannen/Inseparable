080ccbcd2ef56c64674b3a724e56d5478f111b5a
Daniel Nordahl <danielnordahl93@gmail.com>
2018-12-03 15:10:41 +0100

    Small fixes and cleanup

---
+++ Assets/Scripts/Main/DeathArea.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/* Script made by Daniel */
+public class DeathArea : MonoBehaviour {
+
+    LevelManager levelManager;
+    Transform player1;
+    Transform player2;
+    Transform rope;
+
+    private void Start()
+    {
+        levelManager = GameObject.Find("Loading Screen").GetComponent<LevelManager>();
+        player1 = GameObject.Find("Player 1").transform;
+        player2 = GameObject.Find("Player 2").transform;
+        rope    = GameObject.Find("Rope").transform;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            //foreach (Transform t in rope) Destroy(t.gameObject);
+            levelManager.Load("Main");
+            /*player1.position = new Vector3(-2, 0.3f, 0);
+            player2.position = new Vector3(2, 0.3f, 0);
+            rope.GetComponent<RopeCreator>().CreateRope();*/
+        }
+    }
+}

75adbdfff5ad72ca64f1f18e7fd2cdac65cc40eb
Adam <adam.torngren@hotmail.com>
2018-12-06 16:08:50 +0100

    Added movable lever and Island

---
+++ Assets/LeverAction.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Made by Adam */
+public class LeverAction : MonoBehaviour
+{
+    public virtual void LeverPulled(bool leverState){}
+}

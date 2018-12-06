419b834627e23e663d995630c7ebf07f0c8a8783
Joakim Svensson <jkm.svensson@gmail.com>
2018-12-07 00:03:36 +0100

    Respawn PhysObject

--- Assets/RespawnableObject.cs
+++ Assets/RespawnableObject.cs
@@ -1,30 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RespawnableObject : MonoBehaviour {
-    public Vector2 pos;
-    public Vector2 size;
-    public bool drawGizmos;
+    [SerializeField] Transform spawnPoint;
 
-    //Vector2 startPos;
-
-    void Start ()
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        //startPos = transform.position;
-	}
-
-
-    void Update()
-    {
-
-    }
-
-    private void OnDrawGizmos()
-    {
-        if (drawGizmos)
+        if (other.transform.CompareTag("PhysicalObject"))
         {
-            Gizmos.DrawCube(new Vector2(transform.position.x + pos.x, transform.position.y + pos.y), size);
+            other.transform.position = spawnPoint.position;
         }
     }
 }

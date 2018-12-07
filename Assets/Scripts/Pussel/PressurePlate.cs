95a0244cefd893f0d68145e3ad83a82e1f61b899
Daniel Nordahl <danielnordahl93@gmail.com>
2018-12-07 13:45:05 +0100

    Cleanup

--- Assets/Scripts/Pussel/PressurePlate.cs
+++ Assets/Scripts/Pussel/PressurePlate.cs
@@ -2,27 +2,30 @@
 
 /* Made by Daniel */
 public class PressurePlate : MonoBehaviour {
-
-    public string[] tags;
+    public string[] affectedTags;
     public bool on;
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        //if (other.tag == "Player" || other.tag == "PhysicalObject")
-        if (other.tag == tags[0])
+        foreach (string tag in affectedTags)
         {
-            on = true;
-            transform.localScale = new Vector3(transform.localScale.x, 0.1f, transform.localScale.z);
+            if (other.tag == tag)
+            {
+                on = true;
+                transform.localScale = new Vector3(transform.localScale.x, 0.1f, transform.localScale.z);
+            }
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        //if (other.tag == "Player" || other.tag == "PhysicalObject")
-        if (other.tag == tags[0])
+        foreach (string tag in affectedTags)
         {
-            on = false;
-            transform.localScale = new Vector3(transform.localScale.x, 0.2f, transform.localScale.z);
+            if (other.tag == tag)
+            {
+                on = false;
+                transform.localScale = new Vector3(transform.localScale.x, 0.2f, transform.localScale.z);
+            }
         }
     }
 }

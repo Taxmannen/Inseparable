d524cb2e1735b49f0f214b4920a07de962d8bf9b
Daniel Nordahl <danielnordahl93@gmail.com>
2018-12-07 13:36:56 +0100

    Fixed collision bugg

--- Assets/Scripts/Objects/RespawnableObject.cs
+++ Assets/Scripts/Objects/RespawnableObject.cs
@@ -3,7 +3,7 @@
 public class RespawnableObject : MonoBehaviour {
     [SerializeField] Transform spawnPoint;
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.transform.CompareTag("PhysicalObject"))
         {

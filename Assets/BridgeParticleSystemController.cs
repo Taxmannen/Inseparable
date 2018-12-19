9e029b50fffd5c8fd082331c38d380393805d554
Unknown <jkm.svensson@gmail.com>
2018-12-19 11:47:44 +0100

    Bridge particle

---
+++ Assets/BridgeParticleSystemController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeParticleSystemController : MonoBehaviour
+{
+    public new ParticleSystem particleSystem;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       if(other.name == "Bridge") particleSystem.Play();
+    }
+}
---
+++ Assets/Spikes.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    public float startScale = 0.1f;
+    
+}

67709628db171ed7315138142bf8f98a1cbb1fad
Unknown <jkm.svensson@gmail.com>
2018-12-19 17:05:26 +0100

    Spikes

--- Assets/Spikes.cs
+++ Assets/Spikes.cs
@@ -4,6 +4,42 @@ using UnityEngine;
 
 public class Spikes : MonoBehaviour
 {
-    public float startScale = 0.1f;
-    
+    float timer;
+    float y;
+    bool grow;
+    bool shrink;
+
+    private void Start()
+    {
+        grow = false;
+        shrink = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if(timer < 0)
+        {
+            grow = true;
+            shrink = false;
+        }
+
+        if (timer > 2)
+        {
+            grow = false;
+            shrink = true;
+        }
+
+        if (grow)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if(shrink)
+        {
+            timer -= Time.deltaTime;
+        }
+        y = timer;
+        transform.localScale = new Vector3(1, y, 1);
+    }
+
 }

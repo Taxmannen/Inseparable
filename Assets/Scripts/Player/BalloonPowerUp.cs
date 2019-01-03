4b90137fb4a52a343f8a19e3fd39e0915435cfe2
Daniel Nordahl <danielnordahl93@gmail.com>
2019-01-03 15:57:05 +0100

    New logo and fixed balloon bug

--- Assets/Scripts/Player/BalloonPowerUp.cs
+++ Assets/Scripts/Player/BalloonPowerUp.cs
@@ -5,14 +5,16 @@ public class BalloonPowerUp : MonoBehaviour {
     public bool on;
     public Vector2 force;
 
-    Rigidbody2D rb;
-    string button;
-
+    MovementController mc;
     Light characterLight;
+    Rigidbody2D rb;
+
+    string button;
     float lightSize;
     bool inflated;
     bool empty;
-    MovementController mc;
+    bool started;
+
 
 	void Start ()
     {
@@ -21,6 +23,7 @@ public class BalloonPowerUp : MonoBehaviour {
         characterLight = GetComponentInChildren<Light>();
         lightSize = characterLight.range;
         button = "Seperate" + " " + gameObject.name + " " + Main.controllers[transform.GetSiblingIndex()];
+        started = false;
     }
 	
 	void Update ()
@@ -28,22 +31,34 @@ public class BalloonPowerUp : MonoBehaviour {
         float x = Input.GetAxis("Horizontal" + " " + gameObject.name)/8; 
         float y = Input.GetAxis("Vertical"   + " " + gameObject.name)/10;
 
-        if (Input.GetButtonDown(button))
+        if (Input.GetButtonDown(button) && !started)
         {
             on = !on;
         }
         if (on)
         {
-            if (!inflated)
+            if (!inflated && transform.localScale.y < 2)
             {
-                if (mc.facingRight) transform.localScale = new Vector3(transform.localScale.x + 0.025f, transform.localScale.y + 0.025f, 1);
-                else                transform.localScale = new Vector3(transform.localScale.x - 0.025f, transform.localScale.y + 0.025f, 1);
+                started = true;
+                if (mc.facingRight)
+                {
+                    transform.localScale = new Vector3(transform.localScale.x + 0.025f, transform.localScale.y + 0.025f, 1);
+                    if (transform.localScale.y > 2) transform.localScale = new Vector3(2, 2, 2);
+                }
+                else
+                {
+                    transform.localScale = new Vector3(transform.localScale.x - 0.025f, transform.localScale.y + 0.025f, 1);
+                    if (transform.localScale.y > 2) transform.localScale = new Vector3(-2, 2, 2);
+                }
+
                 if (characterLight.range < lightSize * 4) characterLight.range += 0.2f;
                 if (characterLight.range > lightSize * 4) characterLight.range = lightSize * 4;
-                if (transform.localScale.x >= 2 || transform.localScale.x <= -2)
+
+                if (transform.localScale.x == 2 || transform.localScale.x == -2)
                 {
                     inflated = true;
                     empty = false;
+                    started = false;
                 }
             }
             rb.gravityScale = 0;
@@ -53,24 +68,26 @@ public class BalloonPowerUp : MonoBehaviour {
         {
             if (!empty && transform.localScale.y > 1)
             {
+                started = true;
                 if (mc.facingRight)
                 {
                     transform.localScale = new Vector3(transform.localScale.x - 0.025f, transform.localScale.y - 0.025f, 1);
-                    if (transform.localScale.x < 1) transform.localScale = new Vector3(1, 1, 1);
+                    if (transform.localScale.y < 1) transform.localScale = new Vector3(1, 1, 1);
                 }
                 else
                 {
                     transform.localScale = new Vector3(transform.localScale.x + 0.025f, transform.localScale.y - 0.025f, 1);
-                    if (transform.localScale.x > -1) transform.localScale = new Vector3(-1, 1, 1);
+                    if (transform.localScale.y < 1) transform.localScale = new Vector3(-1, 1, 1);
                 }
 
-                if (characterLight.range > lightSize) characterLight.range -= 0.3f;
+                if (characterLight.range > lightSize) characterLight.range -= 0.2f;
                 if (characterLight.range < lightSize) characterLight.range = lightSize;
 
                 if (transform.localScale.x == 1 || transform.localScale.x == -1)
                 {
                     empty = true;
                     inflated = false;
+                    started = false;
                 }
             }
             rb.gravityScale = 1;

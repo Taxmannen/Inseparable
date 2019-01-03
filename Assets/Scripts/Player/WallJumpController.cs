45eeedd796f118ad0730c144742a6d5b5442c09a
Adam <adam.torngren@hotmail.com>
2019-01-03 15:37:01 +0100

    Walljump movement reduction

--- Assets/Scripts/Player/WallJumpController.cs
+++ Assets/Scripts/Player/WallJumpController.cs
@@ -3,9 +3,7 @@ using UnityEngine;
 /* Script made by Adam */
 public class WallJumpController : MovementScript
 {
-    //[Range(50, 100)]
     public float maxGroundSpeed;
-    //[Range(50, 100)]
     public float flatGroundMultiplier;
 
     bool jumpButton;
@@ -30,15 +28,6 @@ public class WallJumpController : MovementScript
                     || Physics2D.OverlapBox(new Vector2(transform.position.x + 0.35f, transform.position.y), new Vector2(0.1f, 0.5f), 0, groundLayer);
 
         jumpButton = Input.GetButton(jumpButtonStr);
-
-        if (jumpButton && wallContact)
-        {
-            jumpTimeCounter = jumpTime;
-            AudioManager.Play("WallJump");
-        }
-            
-        else
-            jumpTimeCounter = 0;
     }
 
     void FixedUpdate()
@@ -56,9 +45,6 @@ public class WallJumpController : MovementScript
                     Debug.Log("Limiting!");
                 }
             }
-            //Debug.Log("Walljumping!");
-            //rb.AddForce(-Physics2D.gravity.normalized * jumpForce + Physics2D.gravity.normalized * (jumpForceReduction * (1 - (jumpTimeCounter / jumpTime))));
-            
             rb.AddForce(-Physics2D.gravity.normalized * (maxGroundSpeed - Mathf.Abs(rb.velocity.y)) * Time.deltaTime * flatGroundMultiplier, 0);
         }
     }

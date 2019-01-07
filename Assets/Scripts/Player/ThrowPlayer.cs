62340477724781414bdb67ef9f8f59545a3556da
Adam <adam.torngren@hotmail.com>
2019-01-07 12:05:28 +0100

    Hopefully no more derp flapping

--- Assets/Scripts/Player/ThrowPlayer.cs
+++ Assets/Scripts/Player/ThrowPlayer.cs
@@ -116,9 +116,10 @@ public class ThrowPlayer : MovementScript {
         else
         {
             i = 0;
-            rightArm.transform.localPosition = rightArmStartPosition;
+            rightArm.transform.localPosition = rightArmStartPosition * rightArmParent.localScale.x;
             rightArm.transform.localEulerAngles = rightArmStartAngles;
-            leftArm.transform.localPosition = leftArmStartPosition * player.localScale.x * -1f;
+            Transform otherPlayer = GetPlayer.getOtherPlayerByName(gameObject.name);
+            leftArm.transform.localPosition = leftArmStartPosition;
             leftArm.transform.localEulerAngles = leftArmStartAngles;
         }
     }

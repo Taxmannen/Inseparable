f75f5bd9411b4db73f6a0cfc324ff3db8c4cd977
Unknown <mikhak1103@skola.goteborg.se>
2019-01-07 16:18:02 +0100

    Rolling Rock Obstacle

---
+++ Assets/StoneRoll.cs
@@ -0,0 +1,42 @@
+﻿    using UnityEngine;
+
+    public class StoneRoll : MonoBehaviour
+    {
+        public float force;
+        Rigidbody2D rb;
+        DamageToPlayer dmg;
+        bool canHurtPlayer = true;
+        public CircleCollider2D colli;
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            Debug.Log(gameObject.name);
+            dmg = GetComponentInChildren<DamageToPlayer>();
+        }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Mathf.Abs(rb.velocity.x));
+
+        if (Mathf.Abs(rb.velocity.x) <= 0.5f) colli.enabled = false;
+        else                                  colli.enabled = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            rb.AddForce(new Vector2(-force, 0), ForceMode2D.Impulse);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+                
+        }
+    }
+}

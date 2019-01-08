b20d0d088475bb68562aa93177ac8222c14cc5e4
Daniel Nordahl <danielnordahl93@gmail.com>
2019-01-08 17:08:32 +0100

    Added more to Level 2

--- Assets/Scripts/Objects/RespawnableObject.cs
+++ Assets/Scripts/Objects/RespawnableObject.cs
@@ -9,30 +9,6 @@ public class RespawnableObject : MonoBehaviour
     public bool touchedByPlayer;
     Vector2 speed;
 
-    //Rigidbody2D rb;
-
-    private void Start()
-    {
-        //rb = GetComponent<Rigidbody2D>();
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        /*
-        if (other.transform.CompareTag("PhysicalObject"))
-        {
-            other.transform.position = spawnPoint.position;
-        }
-        */
-
-       /* if (other.gameObject.name == "Player 1" || other.gameObject.name == "Player 2")
-        {
-            AudioManager.Play("ObjectCollision");
-        }
-        */
-    }
-
-
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Ground")
@@ -46,14 +22,5 @@ public class RespawnableObject : MonoBehaviour
                 }
             }            
         }
-    }
-
-
-    private void Update()
-    {
-        /*
-        speed = rb.GetPointVelocity(transform.TransformPoint(new Vector2(transform.position.x, transform.position.y)));
-        Debug.Log(speed.y);
-        */
     }
 }

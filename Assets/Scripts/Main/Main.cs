5e63f2893690f38eb3c47da7b6e365d9d37401ef
Daniel Nordahl <danielnordahl93@gmail.com>
2019-01-10 08:15:41 +0100

    Small fix

--- Assets/Scripts/Main/Main.cs
+++ Assets/Scripts/Main/Main.cs
@@ -12,8 +12,8 @@ public class Main : MonoBehaviour {
         Application.targetFrameRate = frameRate;
         QualitySettings.vSyncCount = 0;
 
-        /*if (!Application.isEditor) Cursor.visible = false;
-        else                       Cursor.visible = true;*/
+        if (!Application.isEditor) Cursor.visible = false;
+        else                       Cursor.visible = true;
 
         string[] names = Input.GetJoystickNames();
         for (int x = 0; x < 2; x++)
